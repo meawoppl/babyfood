632ff9255c280bdddabc4f25f5454d23e9f60260
MattyG <meawoppl@gmail.com>
2014-08-13 18:57:14 -0700

    Major changes.  Checkin of code I used to send off the test board.

--- PCBBase.py
+++ PCBBase.py
@@ -1,11 +1,11 @@
-import os
+import itertools, os
 import numpy as np
 
 from GerberWriter import GerberWriter
 from DrillWriter import DrillWriter
 
 
-class PCBFeature:
+class TransformableFeature:
     def __init__(self):
         self.x = 0
         self.y = 0
@@ -29,50 +29,41 @@ class PCBFeature:
         txs, tys = m * lhs
         return np.array(txs + self.x).flatten(), np.array(tys + self.y).flatten()
 
-    def drawTopOverlay(self, gerberWriter):
-        pass
 
-    def drawTopMask(self, gerberWriter):
-        pass
+class PCBFeature(TransformableFeature):
+    def __init__(self):
+        TransformableFeature.__init__(self)
+        self.artists = {}
+        self.flipped = False
 
-    def drawTopLayer(self, gerberWriter):
-        pass
+    def setLayerArtist(self, layerSide, layerType, artistFunc):
+        self.artists[(layerSide, layerType)] = artistFunc
 
-    def drawBottomLayer(self, gerberWriter):
-        pass
+    def getLayerArtist(self, layerSide, layerType):
+        assert layerSide in ["Top", "Bottom"]
 
-    def drawBottomMask(self, gerberWriter):
-        pass
+        sideMap = self._switcher(layerSide)
+        return self.artists[(sideMap, layerType)]
 
-    def drawBottomOverlay(self, gerberWriter):
-        pass
+    def setFlip(self, flipped):
+        self.flipped = flipped
 
-    def drawDrills(self, drillFileWriter):
-        pass
+    def _switcher(self, torb):
+        if self.flipped:
+            return {"Top": "Bottom", "Bottom": "Top"}[torb]
+        else:
+            return torb
 
+boardSides = ["Top", "Bottom"]
+layerTypes = ["Layer", "Mask", "Overlay"]
+defaultLayers = list(itertools.product(boardSides, layerTypes))
 
-class SingleSideComponent(PCBFeature):
-    pass
-
-
-class TopSideFeature(SingleSideComponent):
-    pass
-
-
-class BottomSideFeature(SingleSideComponent):
-    pass
-
-
-class DualSideSymmFeature(PCBFeature):
-    pass
-
-
-class DualSideAsymFeature(PCBFeature):
-    pass
-
-
-nameToExtension = {"TopOverlay": "GTO", "TopMask": "GTS", "TopLayer": "GTL",
-                   "BottomLayer": "GBL", "BottomMask": "GBS", "BottomOverlay": "GBO"}
+layerTupleToExtension = {("Top", "Overlay"): "GTO",
+                         ("Top", "Mask"): "GTS",
+                         ("Top", "Layer"): "GTL",
+                         ("Bottom", "Layer"): "GBL",
+                         ("Bottom", "Mask"): "GBS",
+                         ("Bottom", "Overlay"): "GBO"}
 
 
 extensionToColor = {"GTL": "#FF0000", "GBL": "#0000FF",
@@ -81,31 +72,50 @@ extensionToColor = {"GTL": "#FF0000", "GBL": "#0000FF",
 
 
 class PCBDrawer:
-    def __init__(self, folderPath, filename="pcb"):
+    def __init__(self, folderPath, filename="pcb", layers=defaultLayers):
         if not os.path.isdir(folderPath):
             os.makedirs(folderPath)
 
         self.pth = os.path.join(folderPath, filename)
         self.layerWriters = {}
-        for name, ext in nameToExtension.items():
-            gw = GerberWriter(self.pth + "." + ext)
+        for layerTuple, layerExt in layerTupleToExtension.items():
+            gw = GerberWriter(self.pth + "." + layerExt)
             gw.writeGerberHeader()
-            gw.writeLayerPolarity("C" if "Mask" in name else "D")
-            self.layerWriters[name] = gw
+            gw.writeLayerPolarity()
+            self.layerWriters[layerTuple] = gw
 
-        self.drillWriter = DrillWriter(folderPath + ".drl")
+        # Add also teh drill writer
+        dw = DrillWriter(self.pth + ".XLN")
+        self.layerWriters[("Drill", "Drill")] = dw
+
+        self.outlined = False
+
+    def drawOutline(self, pt1, pt2):
+        x1, y1 = pt1
+        x2, y2 = pt2
+
+        gw = GerberWriter(self.pth + ".GKO")
+        gw.writeGerberHeader()
+        gw.writeLayerPolarity()
+
+        gw.defineAperature(0.1, setAsCurrent=True)
+        gw.moveTo(x1, y1)
+        gw.lineTo(x1, y2)
+        gw.lineTo(x2, y2)
+        gw.lineTo(x2, y1)
+        gw.lineTo(x1, y1)
+
+        gw.finalize()
+
+        self.outlined = True
 
     def addFeature(self, pcbFeature):
-        for layerName, layerWriter in self.layerWriters.items():
-            getattr(pcbFeature, "draw" + layerName)(layerWriter)
-
-        pcbFeature.drawDrills(self.drillWriter)
+        for layerTuple, artist in pcbFeature.artists.items():
+            artist(self.layerWriters[layerTuple])
 
     def finalize(self):
         for layerWriter in self.layerWriters.values():
             layerWriter.finalize()
-
-        self.drillWriter.finalize()
 
     def visualize(self):
         filenames = []
@@ -116,7 +126,7 @@ class PCBDrawer:
 
         # Add the drill file
         colors.append("#EEEEEE")
-        filenames.append(self.pth + ".drl")
+        filenames.append(self.pth + ".XLN")
 
         colorString = ""
         for c in colors:
@@ -124,77 +134,8 @@ class PCBDrawer:
 
         filestring = " ".join(filenames)
         gerbCallString = "gerbv " + colorString + " " + filestring
+
+        if self.outlined:
+            gerbCallString += " " + self.pth + ".GKO"
         print(gerbCallString)
         os.system(gerbCallString)
-
-# From https://www.digikey.com/Web%20Export/Supplier%20Content/Vishay_8026/PDF/VishayBeyschlag_SolderPad.pdf?redirected=1
-
-resistorsParams = {
-    "0102": (0.65, 1.10, 1.40, 2.85),
-    "0204": (1.50, 1.25, 1.75, 4.00),
-    "0207": (2.80, 2.20, 2.20, 7.20),
-    "0402": (0.25, 0.60, 0.55, 1.45),
-    "0603": (0.50, 0.95, 0.95, 2.40),
-    "0805": (0.65, 1.10, 1.40, 2.85),
-    "1206": (1.50, 1.25, 1.75, 4.00)}
-
-
-class StdSMAResistor(TopSideFeature):
-    def __init__(self, codeString):
-        self.x = 0
-        self.y = 0
-        self.phi = 0
-
-        assert codeString in resistorsParams, "Wtf ^^"
-        # MRG TODO: MM assertion
-        self.g, self.y, self.x, self.z = resistorsParams[codeString]
-
-        self.width = int(codeString[0:2]) * 0.254
-        self.height = int(codeString[2:4]) * 0.254
-
-    def computePadCoords(self, buff=0):
-        x1 = (self.g / 2) - buff
-        x2 = (self.z / 2) + buff
-
-        y1 = (-self.x / 2) - buff
-        y2 = (self.x / 2) + buff
-
-        coords = []
-        coords.append((x1, y1))
-        coords.append((x1, y2))
-        coords.append((x2, y2))
-        coords.append((x2, y1))
-        coords.append((x1, y1))
-        return np.array(coords).T
-
-    def drawTopMask(self, gerberWriter):
-        xs1, ys1 = np.array(self.computePadCoords(buff=0))
-        # Pad two is just mirrored wrt to y-axis
-        xs2, ys2 = -xs1.copy(), ys1.copy()
-
-        xs1, ys1 = self.transform(xs1, ys1)
-        xs2, ys2 = self.transform(xs2, ys2)
-
-        gerberWriter.simplePolygon(xs1, ys1)
-        gerberWriter.simplePolygon(xs2, ys2)
-
-    def drawTopLayer(self, gerberWriter):
-        xs1, ys1 = np.array(self.computePadCoords(buff=0.1))
-        # Pad two is just mirrored wrt to y-axis
-        xs2, ys2 = -xs1.copy(), ys1.copy()
-
-        xs1, ys1 = self.transform(xs1, ys1)
-        xs2, ys2 = self.transform(xs2, ys2)
-
-        gerberWriter.simplePolygon(xs1, ys1)
-        gerberWriter.simplePolygon(xs2, ys2)
-
-
-if __name__ == "__main__":
-    pcb = PCBDrawer("testing")
-    r = StdSMAResistor("0805")
-    r.setRotation(np.pi / 7)
-    pcb.addFeature(r)
-
-    pcb.finalize()
-    pcb.visualize()
