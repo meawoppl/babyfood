316b6178882590d040390131ccd9e2a1bc71e966
meawoppl <meawoppl@gmail.com>
2014-12-23 21:10:09 -0800

    Minimum module packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='babyfood',
+      version='0.0.0',
+      description='PCB Tools',
+      author='Matthew Goodman',
+      author_email='meawoppl@gmail.com',
+      url='https://www.github.com/babyfood',
+      packages=['babyfood'],
+      )
