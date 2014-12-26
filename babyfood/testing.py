c60f0f923f0e76bdacc6c55727495ee7702d8d4b
meawoppl <meawoppl@gmail.com>
2014-12-26 15:49:50 -0800

    basic test.

--- babyfood/testing.py
+++ babyfood/testing.py
@@ -1,7 +1,9 @@
-import sys
+import unittest, sys
 
 def test():
-    return True
+    loader = unittest.TestLoader()
+    suite = loader.discover("babyfood.tests")
+    return unittest.main().runTests(suite=suite)
 
 if __name__ == "__main__":
     sys.exit(0 if test() else 1)
