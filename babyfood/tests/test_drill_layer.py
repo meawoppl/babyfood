289f18839ace6ae6f773a2de40d6c46ded49c499
meawoppl <meawoppl@gmail.com>
2015-01-10 13:37:31 -0800

    Actully test drill layer

--- babyfood/tests/test_drill_layer.py
+++ babyfood/tests/test_drill_layer.py
@@ -1,10 +1,10 @@
-from babyfood.layers.GerberLayer import GerberLayer
+from babyfood.layers.DrillLayer import DrillLayer
 from babyfood.tests.shared import GerbvTester, _quickTempFilePath
 
 
 class GerberWriterTestCase(GerbvTester):
     def test_drill_empty(self):
         xlnFilePath = _quickTempFilePath(".xln")
-        xw = GerberLayer(xlnFilePath)
+        xw = DrillLayer(xlnFilePath)
         xw.finalize()
         self._check_gerber_file(xlnFilePath)
