e9080bc0a6e624bc9ba7d8506ddf591bc9d0c311
meawoppl <meawoppl@gmail.com>
2015-02-22 15:52:18 -0800

    Add redius com.  Rect features.

--- babyfood/features/basic.py
+++ babyfood/features/basic.py
@@ -1,11 +1,13 @@
-class CenteredRectangle:
+class AbstractRectangularFeature:
     def __init__(self, w, h):
         assert w > 0
         assert h > 0
         self._hw = w / 2
         self._hh = h / 2
 
-    def draw(self, ctx):
+
+class CenteredRectangle(AbstractRectangularFeature):
+    def _outline(self, ctx):
         ctx.moveTo(-self._hw, self._hh)
 
         ctx.lineTo(self._hw, self._hh)
@@ -13,11 +15,23 @@ class CenteredRectangle:
         ctx.lineTo(-self._hw, -self._hh)
         ctx.lineTo(-self._hw, self._hh)
 
+    def draw(self, ctx):
+        self._outline(ctx)
+
 
 class FilledCenteredRectangle(CenteredRectangle):
     def draw(self, ctx):
         with ctx.polygonMode():
-            CenteredRectangle.draw(self, ctx)
+            self._outline(ctx)
+
+
+class CenteredCircle:
+    def __init__(self, r):
+        assert r > 0
+        self._r = r
+
+    def draw(self, ctx):
+        ctx.circle(0, 0, self._r)
 
 
 class FilledCenteredCircle:
@@ -28,6 +42,11 @@ class FilledCenteredCircle:
     def draw(self, ctx):
         with ctx.polygonMode():
             ctx.circle(0, 0, self._r)
+
+
+class Obround(AbstractRectangularFeature):
+    def draw(self, ctx):
+        raise NotImplemented
 
 
 class CircularVia:
@@ -47,4 +66,4 @@ class CircularVia:
                 ctx.setActiveLayer(layer)
                 fcc.draw(ctx)
         # Drill the hole
-        ctx.addHole(0, 0, self._hr)
+        ctx.addHole(0, 0, self._hr * 2)
